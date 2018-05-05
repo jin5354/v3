4091a94c97e5f0da713732060a65c74bfbfae560
jin5354 <xiaoyanjinx@gmail.com>
2018-05-05 16:07:53 +0800

    [feat] add webpack to pack umd module.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import Vector3 from './Vector3'
 import EulerAngles from './EulerAngles'
-import Matrix4x3 from './Matrix4x3'
 import RotationMatrix from './RotationMatrix'
+import Matrix4x3 from './Matrix4x3'
 import Quaternion from './Quaternion'
 
-export default {
+export {
   Vector3,
   EulerAngles,
   Matrix4x3,
