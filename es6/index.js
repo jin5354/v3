8626c9c87bf5db9a0e5183f010da5e80c5c9a4de
jin5354 <xiaoyanjinx@gmail.com>
2018-05-16 14:46:55 +0800

    [release] release v0.3.1

--- es6/index.js
+++ es6/index.js
@@ -3,4 +3,5 @@ import EulerAngles from './EulerAngles';
 import RotationMatrix from './RotationMatrix';
 import Matrix4x3 from './Matrix4x3';
 import Quaternion from './Quaternion';
-export { Vector3, EulerAngles, Matrix4x3, RotationMatrix, Quaternion };
+import Matrix4 from './Matrix4';
+export { Vector3, EulerAngles, Matrix4x3, RotationMatrix, Quaternion, Matrix4 };
