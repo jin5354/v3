b625e55d74e9a53b91be71182c1d48368b334e91
jin5354 <xiaoyanjinx@gmail.com>
2018-06-19 19:07:27 +0800

    [chore] remove dist

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import Vector3 from './Vector3';
-import EulerAngles from './EulerAngles';
-import Quaternion from './Quaternion';
-import Matrix4 from './Matrix4';
-export { Vector3, EulerAngles, Quaternion, Matrix4 };
