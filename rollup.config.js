404dfff36323198af354ddbd7d266a8f94359f05
jin5354 <xiaoyanjinx@gmail.com>
2020-02-29 16:52:08 +0800

    build(rollup): change webpack to rollup

---
+++ rollup.config.js
@@ -0,0 +1,63 @@
+import typescript from 'rollup-plugin-typescript2'
+import babel from 'rollup-plugin-babel'
+import {terser} from 'rollup-plugin-terser'
+
+// lib/index.js-commonjs-main   标准使用
+// dist/index.js-umd-unpkg      全局引用
+// es/index.js-es-module        tree-shaking用
+export default [{
+  input: './src/index.ts',
+  output: {
+    file: './dist/index.js',
+    format: 'umd',
+    name: 'v3js',
+  },
+  plugins: [
+    typescript({
+      clean: true,
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: true
+        }
+      }
+    }),
+    babel({ runtimeHelpers: true }),
+    //terser(),
+  ]
+}, {
+  input: './src/index.ts',
+  output: {
+    file: './lib/index.js',
+    format: 'cjs',
+  },
+  plugins: [
+    typescript({
+      clean: true,
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: true
+        }
+      }
+    }),
+    babel({ runtimeHelpers: true }),
+    //terser(),
+  ]
+}, {
+  input: './src/index.ts',
+  output: {
+    file: './es/index.js',
+    format: 'es',
+  },
+  plugins: [
+    typescript({
+      clean: true,
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: true
+        }
+      }
+    }),
+    babel({ runtimeHelpers: true }),
+    //terser(),
+  ]
+}]
